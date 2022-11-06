1265f0a0f1d1adb2d6408c41d720381bffafb4e6
GabrielTury <gabrieltury@gmail.com>
2022-11-06 14:46:42 -0300

    Dialogue system working

---
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public TextMeshProUGUI dialogueText;
+    public GameObject dialoguePrefab;
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        dialoguePrefab.SetActive(true);
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(UpdateDialogue(sentence));
+    }
+
+    IEnumerator UpdateDialogue(string sentence)
+    {
+        dialogueText.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(0.01f);
+        }
+    }
+
+    private void EndDialogue()
+    {
+        dialoguePrefab.SetActive(false);
+    }
+}
